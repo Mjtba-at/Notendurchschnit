f8eb20cecdc4ce14876e7b407a962721e1a592b9
Mjtba-at <148459044+Mjtba-at@users.noreply.github.com>
2023-11-13 17:10:27 +0000

    Tests for Service

--- nd/src/main/java/edu/fra/uas1/nd/controller/NotenController.java
+++ nd/src/main/java/edu/fra/uas1/nd/controller/NotenController.java
@@ -2,7 +2,6 @@ package edu.fra.uas1.nd.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,11 +22,11 @@ public class NotenController {
         return notenService.berechnenDurchschnitt(noten);
     }
     // Test-Endpunkt nur für Testzwecke
-    @RequestMapping(value = "/test/durchschnitt", method = RequestMethod.GET)
-    @ResponseBody
-    public double testNotenDurchschnitt() {
+   // @RequestMapping(value = "/test/durchschnitt", method = RequestMethod.GET)
+  //  @ResponseBody
+   // public double testNotenDurchschnitt() {
         // Testdaten hier zuweisen
-        int[] testNoten = {80, 75, 90, 85};
-        return notenService.berechnenDurchschnitt(testNoten);
-    }
+    //    int[] testNoten = {80, 75, 90, 85};
+      //  return notenService.berechnenDurchschnitt(testNoten);
+    //}
 }
--- nd/src/test/java/edu/fra/uas1/nd/NotenServiceTests.java
+++ nd/src/test/java/edu/fra/uas1/nd/NotenServiceTests.java
@@ -1,23 +1,24 @@
 package edu.fra.uas1.nd;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import edu.fra.uas1.nd.controller.NotenController;
-import static org.assertj.core.api.Assertions.assertThat;
+import edu.fra.uas1.nd.notenService.NotenService;
 
 @SpringBootTest
-public class NotenControllerTests {
+public class NotenServiceTests {
 
     @Autowired
-    private NotenController notenController;
+    private NotenService notenService;
 
     @Test
     public void testNotenDurchschnitt() {
         int[] noten = {80, 75, 90, 85, 70};
         double expectedDurchschnitt = 80.0;
 
-        double actualDurchschnitt = notenController.notenDurchschnitt(noten);
+        double actualDurchschnitt = notenService.berechnenDurchschnitt (noten);
 
         assertThat(actualDurchschnitt).isEqualTo(expectedDurchschnitt);
     }
